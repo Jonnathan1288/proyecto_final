f52c118424bdc2901b237f986ae9a4ef0ae39272
DELL <DELL@DESKTOP-9147F0D>
2021-10-02 15:26:45 -0500

    Clase nueva prueba

---
+++ src/Comportamientos/Prueba.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Comportamientos;
+
+/**
+ *
+ * @author DELL
+ */
+public class Prueba {
+    String nom;
+
+    public Prueba() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    
+}
