a8b2778782ddf82351dbfb52811d192ba95fe5fb
DELL <DELL@DESKTOP-9147F0D>
2021-10-01 22:17:55 -0500

    Clase con un nuevo frame

---
+++ src/Comportamientos/pr.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Comportamientos;
+
+/**
+ *
+ * @author DELL
+ */
+public class pr {
+    
+}
